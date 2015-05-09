a1ffca79d2f177167da293818b3c409c8d4e3ccd
Robert Calvert <robertcalvert@users.noreply.github.com>
2015-05-09 15:48:13 +0100

    Open sourced!
    Updated all the file headers...

--- src/io/flob/blackheart/StateSleep.java
+++ src/io/flob/blackheart/StateSleep.java
@@ -5,10 +5,24 @@
  *    \ \_____\  \ \_____\  \ \_\ \_\  \ \_____\  \ \_\ \_\  \ \_\ \_\  \ \_____\  \ \_\ \_\  \ \_\ \_\    \ \_\ 
  *     \/_____/   \/_____/   \/_/\/_/   \/_____/   \/_/\/_/   \/_/\/_/   \/_____/   \/_/\/_/   \/_/ /_/     \/_/ 
  * 
- *      StateSleep.java
+ *      (August 10th-17th 2013) 
+ *      <http://7dfps.calvert.io>
  *
- *      Copyright (c) 2013 Robert Calvert <http://www.facebook.com/robert.calvert>
- *      All Rights Reserved.
+ *      blackheart
+ *      Copyright (c) 2013 Robert Calvert <http://robert.calvert.io>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  */
 package io.flob.blackheart;
